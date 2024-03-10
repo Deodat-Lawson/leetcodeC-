16bdd6191d9880e9bae0512ddc50bf900b58b72b
timot <timothylinziqi@gmail.com>
2024-03-09 23:22:02 -0500

    finished some leetcode questions

---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/timot/CLionProjects/leetcodeC-/binary_tree_level_order_traversal.cpp" "CMakeFiles/untitled2.dir/binary_tree_level_order_traversal.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/binary_tree_level_order_traversal.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/binary_tree_right_side_view.cpp" "CMakeFiles/untitled2.dir/binary_tree_right_side_view.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/binary_tree_right_side_view.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/count_good_nodes_in_binary_tree.cpp" "CMakeFiles/untitled2.dir/count_good_nodes_in_binary_tree.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/count_good_nodes_in_binary_tree.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/lca_of_bst.cpp" "CMakeFiles/untitled2.dir/lca_of_bst.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/lca_of_bst.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/main.cpp" "CMakeFiles/untitled2.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/main.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/subtreeOfAnotherTree.cpp" "CMakeFiles/untitled2.dir/subtreeOfAnotherTree.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/subtreeOfAnotherTree.cpp.obj.d"
+  "C:/Users/timot/CLionProjects/leetcodeC-/validate_binary_search_tree.cpp" "CMakeFiles/untitled2.dir/validate_binary_search_tree.cpp.obj" "gcc" "CMakeFiles/untitled2.dir/validate_binary_search_tree.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2.dir/binary_tree_level_order_traversal.cpp.obj"
+  "CMakeFiles/untitled2.dir/binary_tree_level_order_traversal.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/binary_tree_right_side_view.cpp.obj"
+  "CMakeFiles/untitled2.dir/binary_tree_right_side_view.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/count_good_nodes_in_binary_tree.cpp.obj"
+  "CMakeFiles/untitled2.dir/count_good_nodes_in_binary_tree.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/lca_of_bst.cpp.obj"
+  "CMakeFiles/untitled2.dir/lca_of_bst.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/main.cpp.obj"
+  "CMakeFiles/untitled2.dir/main.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/subtreeOfAnotherTree.cpp.obj"
+  "CMakeFiles/untitled2.dir/subtreeOfAnotherTree.cpp.obj.d"
+  "CMakeFiles/untitled2.dir/validate_binary_search_tree.cpp.obj"
+  "CMakeFiles/untitled2.dir/validate_binary_search_tree.cpp.obj.d"
+  "libuntitled2.dll.a"
+  "untitled2.exe"
+  "untitled2.exe.manifest"
+  "untitled2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
